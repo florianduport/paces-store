f8e46f5aab545d306ef2ee88a693693950436e9c
Florian Duport <florianduport@gmail.com>
2014-10-12 16:57:16 +0200

    animations

---
+++ marketing/paces-store-landing/js/animate.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+	//check animation => lancer les animations sur les éléments directement visibles
+	checkAnimation();
+
+	$(window).scroll( function(){
+		//relancer les animations potentielles à chaque scroll
+		checkAnimation();
+	});
+});
+
+
+var delayedAnimation = function(animationElement, animation, timeout){
+	setTimeout(function(){ 
+		animationElement.removeClass("visibility-hidden");
+		animationElement.addClass(animation+" animated"); 
+	}, timeout);
+}
+
+var checkAnimation = function(){
+	//tous les éléments à animer sont à ajouter ici
+	//cf classes d'animations de ANIMATE.CSS
+	var animationArray = [
+		{selector: ".logo-paces-store", animation: "bounceIn animated", timeout: 1000},
+		{selector: ".newsletter-form", animation: "tada animated", timeout: 1000},
+		{selector: "#video-container", animation: "tada animated", timeout: 0},
+	];
+
+	for(i = 0; i < animationArray.length; i++) {
+
+		var elementArray = $(animationArray[i].selector);
+		
+		if(elementArray.length > 0){
+
+			var element = $(elementArray[0]);
+			var objectBottom = element.position().top + element.outerHeight();
+            var windowBottom = $(window).scrollTop() + $(window).height();
+
+            if( windowBottom > objectBottom ){
+            		delayedAnimation(element, animationArray[i].animation, animationArray[i].timeout !== undefined ? animationArray[i].timeout : 0);
+            }
+
+		}
+		else {
+			console.log("couldn't find element "+animationArray[i].selector);
+		}
+	}
+};
