c996626c49480b89c5202387b3ca63036722cdaa
Florian Duport <florianduport@gmail.com>
2014-11-23 14:58:19 +0100

    mongo session storage

--- marketplace/main.js
+++ marketplace/main.js
@@ -4,6 +4,7 @@ cookieParser = require('cookie-parser'),
 bodyParser = require('body-parser'),
 session = require('express-session'),
 Routes = require('./routes/routes').Routes,
+MongoStore = require('connect-mongo')(session),
 ConfigurationHelper = require('./helpers/configuration.helper').ConfigurationHelper;
 
 /**
@@ -22,11 +23,12 @@ var Main = {
 			// Ne pas toucher ce bloc
 			var app = express();
 			app.use(cookieParser());
-			app.use(session({ 
-				secret: 'paces-store',
-    			maxAge  : new Date(Date.now() + 3600000), //1 Hour
-    			expires : new Date(Date.now() + 3600000)
-    		}));
+    		app.use(session({
+			  store: new MongoStore({
+			    url: configuration.database.address
+			  }),
+			  secret: 'paces-store'
+			}));
 
 			app.set('port', configuration.port);
 			app.set('views', __dirname + '/views');
