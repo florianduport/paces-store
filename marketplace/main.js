90eb6f2f9ab74867cd1709dc4e706fed2a216528
adriend <adrien.dhuicq@viacesi.fr>
2015-02-24 22:43:24 +0100

    handlebars MAJ !

--- marketplace/main.js
+++ marketplace/main.js
@@ -1,56 +1,57 @@
 var express = require('express'),
-path = require('path'),
-cookieParser = require('cookie-parser'),
-bodyParser = require('body-parser'),
-session = require('express-session'),
-Routes = require('./routes/routes').Routes,
-MongoStore = require('connect-mongo')(session),
-ConfigurationHelper = require('./helpers/configuration.helper').ConfigurationHelper;
+        html = require('express-handlebars'),
+        path = require('path'),
+        cookieParser = require('cookie-parser'),
+        bodyParser = require('body-parser'),
+        session = require('express-session'),
+        Routes = require('./routes/routes').Routes,
+        MongoStore = require('connect-mongo')(session),
+        ConfigurationHelper = require('./helpers/configuration.helper').ConfigurationHelper;
 
 /**
  * Classe principale - Keep it simple in here
  * @class Main 
  */
 var Main = {
-
     /**
-    * start : lance l'application express
-    */
-    start : function(){
+     * start : lance l'application express
+     */
+    start: function () {
 
-    	ConfigurationHelper.getConfig({application : 'marketplace', done : function(configuration){
+        ConfigurationHelper.getConfig({application: 'marketplace', done: function (configuration) {
 
-			// Ne pas toucher ce bloc
-			var app = express();
-			app.use(cookieParser());
-    		app.use(session({
-	   			cookie: { maxAge: new Date(Date.now() + 30*24*60*60*1000)},
-				store: new MongoStore({
-					url: configuration.database.address,
-					clear_interval: 3600
-				}, startContent),
-				secret: 'paces-store'
-			}));
+                // Ne pas toucher ce bloc
+                var app = express();
+                app.use(cookieParser());
+                app.use(session({
+                    cookie: {maxAge: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)},
+                    store: new MongoStore({
+                        url: configuration.database.address,
+                        clear_interval: 3600
+                    }, startContent),
+                    secret: 'paces-store'
+                }));
 
-    		function startContent(){
-				app.set('port', configuration.port);
-				app.set('views', __dirname + '/views');
-				app.set('view engine', 'jade');
-				app.use(express.static(path.join(__dirname, 'public')));
+                function startContent() {
+                    app.set('port', configuration.port);
+                    app.set('views', __dirname + '/views');
+                    app.engine('html', html({layoutsDir: path.join(app.settings.views, ""), defaultLayout: 'layout', extname: '.html'}));
+                    app.set('view engine', 'html');
+                    app.use(express.static(path.join(__dirname, 'public')));
 
-				app.use(bodyParser());
+                    app.use(bodyParser());
 
-				Routes.loadRoutes(app, configuration);
+                    Routes.loadRoutes(app, configuration);
 
-				//lancement du serveur
-				app.listen(configuration.port, function(){
-				    console.log('marketplace started on '+(configuration.addressBasePath+":"+configuration.port));      
-				});
-			}
+                    //lancement du serveur
+                    app.listen(configuration.port, function () {
+                        console.log('marketplace started on ' + (configuration.addressBasePath + ":" + configuration.port));
+                    });
+                }
 
-		}});
+            }});
 
-	}
+    }
 
 };
 
