0dab025ca5f79a6848aaf12ba9e1aa358967bcc4
Florian <florianduport@gmail.com>
2015-04-07 22:14:26 +0200

    modif fiche prod

--- marketplace/models/product.model.js
+++ marketplace/models/product.model.js
@@ -4,20 +4,36 @@ var SchoolsHelper = require('../helpers/schools.helper').SchoolsHelper;
 var ProductModel = {
 
 	initialize : function(req, callback){
-
 		ServiceHelper.getService('product', 'getProductById', {data: {"productId" : req.params.product}, method : "POST"}, function(product){
 			if(!product)
 				callback(false);
 			else {
 				this.product = product;
 				var model = this;
-
-				SchoolsHelper.loadSchool(model, { university : SchoolsHelper.loadUniversity(req)}, callback);
+				model.req = req;
+				model.callback = callback;
+				ProductModel._getSeller(model, function(model){
+					SchoolsHelper.loadSchool(model, { university : SchoolsHelper.loadUniversity(model.req)}, model.callback);
+				});
 			}
 		});
-	}
+	},
 
+	_getSeller : function(model, callback){
+		ServiceHelper.getService('seller', 'getSellerByUsername', {data: {"username" : product.seller}, method : "POST"}, function(seller){
+			if(!seller)
+				callback(false);
+			else {
+				model.seller = seller;
+				ServiceHelper.getService('school', 'getSchoolByUrlId', {data: { universityId : model.seller.account.universityId }, method : "POST"}, function(university){
+      				model.seller.account.university = university;
+      				callback(model);
+      			});
 
+				
+			}
+		});
+	},
 };
 
 module.exports.ProductModel = ProductModel;
