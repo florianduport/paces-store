4010f6d0183f1b2b4f6b8262a8482eb0649fdcf3
Florian <florianduport@gmail.com>
2015-05-01 13:14:48 +0200

    keep alive mongodb

--- marketplace/helpers/database.helper.js
+++ marketplace/helpers/database.helper.js
@@ -1,16 +1,26 @@
 var MongoClient = require('mongodb').MongoClient;
 ConfigurationLocalHelper = require('./configuration.local.helper').ConfigurationLocalHelper,
-LocalConfig = require(ConfigurationLocalHelper.getLocalConfigFile(module.parent.parent.id)).LocalConfig,
-DatabaseInstance = undefined; 
+LocalConfig = require(ConfigurationLocalHelper.getLocalConfigFile(module.parent.parent.id)).LocalConfig;
+
 /**
  * Connexion à la BDD
  * @class DatabaseHelper
  */
 var DatabaseHelper = {
 
+    DatabaseInstance : undefined,
+
+    dbOptions : {
+        server: {
+            'auto_reconnect': true,
+            'poolSize': 20,
+            socketOptions: {keepAlive: 1}  
+        }
+    },
+
     getDatabase : function(ToExecute){
-        if(DatabaseInstance !== undefined) {
-            ToExecute(DatabaseInstance);
+        if(DatabaseHelper.DatabaseInstance !== undefined) {
+            ToExecute(DatabaseHelper.DatabaseInstance);
             return;
         } else {
             DatabaseHelper._openNewConnection(ToExecute);
@@ -18,12 +28,12 @@ var DatabaseHelper = {
     },
 
     _openNewConnection : function(ToExecute){
-        MongoClient.connect(LocalConfig.database.address, function(err, db) {
+        MongoClient.connect(LocalConfig.database.address, DatabaseHelper.dbOptions, function(err, db) {
             if(err){
                 console.log(err);
                 DatabaseHelper._openNewConnection(ToExecute);
             } else {
-                DatabaseInstance = db;
+                DatabaseHelper.DatabaseInstance = db;
                 ToExecute(DatabaseInstance);
             }
         });
