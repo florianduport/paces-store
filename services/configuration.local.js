60fd444e191019adc26f7b0b664f3f5f248b915d
Florian Duport <florianduport@gmail.com>
2015-05-08 17:05:11 +0200

    clean configuratio file

--- services/configuration.local.js
+++ services/configuration.local.js
@@ -11,11 +11,9 @@ var LocalConfig = {
 	database : {
 		address : "mongodb://paces-store:Answer&&Pigeon2010@94.23.203.174:27017/paces-store"
 	},
-<<<<<<< HEAD
+	
 	addressBasePath : "http://localhost",
-=======
-	addressBasePath : "http://giantapp.fr",
->>>>>>> preprod
+
 	hmacEnabled : false
 	
 };
