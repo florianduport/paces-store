4c58797b89bd39a7e783e577e34a0f1dc37a2981
florianduport@gmail.com <florianduport@gmail.com>
2016-02-01 19:49:40 +0100

    correction paiement

--- marketplace/controllers/checkout.controller.js
+++ marketplace/controllers/checkout.controller.js
@@ -2,52 +2,60 @@ var model = require('../models/checkout.model').CheckoutModel;
 
 var CheckoutController = {
 
-	initialize : function(req, res){
-		if(req.session.shoppingcart === undefined || req.session.shoppingcart.length == 0){
-			res.redirect('/');
-		} else {
+  initialize: function(req, res) {
+    if (req.session.shoppingcart === undefined || req.session.shoppingcart.length == 0) {
+      res.redirect('/');
+    } else {
 
-		    model.initialize(req, function(model){
-		    	res.render("pages/checkout", {model : model});
-		    });
-		}
-	}, 
-	payWithNewCard : function(req, res){
-            
-                if(req.params.number && req.params.expiry && req.params.cvc && req.params.name){
+      model.initialize(req, function(model) {
+        res.render("pages/checkout", {
+          model: model
+        });
+      });
+    }
+  },
+  payWithNewCard: function(req, res) {
+    if (req.body.number && req.body.expiry && req.body.cvc && req.body.name) {
 
-                    model.payWithNewCard(req, function(model){
-                            if(!model){
-                                    res.render('pages/checkout/error', {model: model});
-                            } else {
-                                    res.redirect('/checkout/wait/'+model.orderId);
-                            }
-                    });
-                } else {
-                    res.redirect('/checkout');
-                }
-	},
+      model.payWithNewCard(req, function(model) {
+        if (!model) {
+          res.render('pages/checkout/error', {
+            model: model
+          });
+        } else {
+          res.redirect('/checkout/wait/' + model.orderId);
+        }
+      });
+    } else {
+      res.redirect('/checkout');
+    }
+  },
 
-	waitPayment : function(req, res){
-		model.waitPayment(req, function(model){
-			if(!model){
-				res.render('pages/checkout/error', {model: model});
-			}
-			if(model.orderConfirmed){
-				res.redirect('/checkout/success/'+model.orderId);
-			} else {
-				res.render('pages/checkout/wait', {model : model});
-			}
-		});
-	},
+  waitPayment: function(req, res) {
+    model.waitPayment(req, function(model) {
+      if (!model) {
+        res.render('pages/checkout/error', {
+          model: model
+        });
+      }
+      if (model.orderConfirmed) {
+        res.redirect('/checkout/success/' + model.orderId);
+      } else {
+        res.render('pages/checkout/wait', {
+          model: model
+        });
+      }
+    });
+  },
 
-	successPayment : function(req, res){
-		model.successPayment(req, function(model){
-			res.render('pages/checkout/success', {model: model});
-		});
-	}
+  successPayment: function(req, res) {
+    model.successPayment(req, function(model) {
+      res.render('pages/checkout/success', {
+        model: model
+      });
+    });
+  }
 
 };
 
 module.exports.CheckoutController = CheckoutController;
-
