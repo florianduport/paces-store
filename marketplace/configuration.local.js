4f3b107af0af0405cfc032bf42f50fac0a9ffd17
jewelz <florianduport@gmail.com>
2016-02-16 19:59:56 +0000

    modif config.local

--- marketplace/configuration.local.js
+++ marketplace/configuration.local.js
@@ -12,8 +12,7 @@ var LocalConfig = {
     address: "mongodb://paces-store:Answer&&Pigeon2010@94.23.203.174:27017/paces-store"
   },
 
-  addressBasePath: "https://paces-store-jewelz.c9users.io",
-  port: 8080,
+  addressBasePath: "http://localhost",
   hmacEnabled: false,
   debugEnabled: false,
   parentFolder: "/home/florian/paces-store/marketplace/"
